ec59951292e78e769df7196b8a720178dd74b3c4
sompaluckyrenklis-design <sompaluckyrenklis@gmail.com>
2026-04-02 21:39:01 +0400

    2eme essaie

--- lib/fonctions.sh
+++
@@ -1,8 +0,0 @@
-# Affiche un résumé système : OS, kernel, uptime, RAM
-sys_summary() {
-  echo "=== Résumé système ==="
-  echo "OS     : $(lsb_release -d | cut -f2)"
-  echo "Kernel : $(uname -r)"
-  echo "Uptime : $(uptime -p)"
-  echo "RAM    : $(free -h | awk '/Mem:/{print $3 " / " $2}')"
-}
